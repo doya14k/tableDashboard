7ecf95e764541ace314b90bb2a065116987fe788
dominic_knoepfel <dominicknoepfel@outlook.com>
2025-10-05 23:13:50 +0200

    setting up the e-paper library

--- Tisch_Dashboard/src/main.cpp
+++ Tisch_Dashboard/src/main.cpp
@@ -1,10 +1,80 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-void setup() {
+// // Beispiel: Touch an GPIO4 (T0)
+// #define TOUCH_PIN T0  // alternativ: 4
 
+// void setup() {
+//   Serial.begin(9600);
+//   delay(1000);
+//   Serial.println("Kapazitive Gestenerkennung gestartet!");
+// }
+
+// void loop() {
+//   int touchValue = touchRead(TOUCH_PIN);
+//   Serial.println(touchValue);
+
+//   if(touchValue < 30) { // Schwellwert anpassen!
+//     Serial.println("Berührung erkannt!");
+//   }
+
+//   delay(100);
+// }
+
+#include "DEV_Config.h"
+#include "EPD.h"
+#include "GUI_Paint.h"
+#include "fonts.h"
+#include <stdlib.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Hello World Demo");
+
+  // Hardware + Display initialisieren
+  DEV_Module_Init();
+  EPD_7IN5_V2_Init();
+  EPD_7IN5_V2_Clear();
+  DEV_Delay_ms(500);
+
+  // Bildspeicher reservieren
+  UBYTE *BlackImage;
+  UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
+  BlackImage = (UBYTE *)malloc(Imagesize);
+  if (BlackImage == NULL)
+  {
+    Serial.println("Memory alloc failed!");
+    while (1)
+      ;
+  }
+
+  // Neues Bild erzeugen & weiß füllen
+  Paint_NewImage(BlackImage, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT, 0, WHITE);
+  Paint_SelectImage(BlackImage);
+  Paint_Clear(WHITE);
+
+  // Text in der Mitte platzieren
+  const char *message = "HELLO DOMINIC";
+  int textX = (EPD_7IN5_V2_WIDTH - (strlen(message) * Font24.Width)) / 2;
+  int textY = (EPD_7IN5_V2_HEIGHT - Font24.Height) / 2;
+  Paint_DrawString_EN(textX, textY, message, &Font24, WHITE, BLACK);
+
+  // Anzeigen
+  EPD_7IN5_V2_Display(BlackImage);
+  DEV_Delay_ms(5000); // 5 Sekunden warten
+
+  for (int i = 0; i < 5; i++)
+  {
+    EPD_7IN5_V2_Clear(); // komplett weiß
+    DEV_Delay_ms(2000);
+  }
+
+  // Speicher freigeben und schlafen legen
+  free(BlackImage);
+  EPD_7IN5_V2_Sleep();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // nichts mehr
 }
-
